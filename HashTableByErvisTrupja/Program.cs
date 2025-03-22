a184eef14852ca7b6b940910f01423920d8a8f54
Kristian Lubina <kristian.lubina@outlook.com>
2025-03-22 22:37:29 +0100

    initialization

---
+++ HashTableByErvisTrupja/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+//the hashtable is used to store key-value pairs
+//key and value can be of any data type
+//there must be a key but value can be null
+namespace HashTableByErvisTrupja
+  {
+  class Program
+    {
+    static void Main( string[] args )
+      {
+      //create hashtable object
+      Hashtable myHashtable = new Hashtable()
+      //first way to intiialize (in the yt-video at 01:43)
+        {
+          { 3, "Three"},
+          { "another", 78.5}
+        };
+      //initialize hashtable and add values
+      myHashtable.Add( 1, "One" );
+      myHashtable.Add( "2", "Two" );
+
+      //print out hashtable content
+      Hashtable myHashtablePrintOut = new Hashtable( myHashtable );
+
+      string valueOfOne = (string) myHashtablePrintOut[ 1 ];
+      Console.WriteLine( $"Value of One = {valueOfOne}" );
+      }
+    }
+  }
